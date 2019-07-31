ea348d0e33df9dcdb69dff6375feb7f0804a2f9a
wangyingbo <wangyingbo528@126.com>
2019-07-31 16:43:30 +0800

    review

--- ReactiveCocoaDemo/Models/School/Grade.m
+++ ReactiveCocoaDemo/Models/School/Grade.m
@@ -10,4 +10,35 @@
 
 @implementation Grade
 
+- (void)fool {
+    
+    if (!method1()) { return; }
+    
+    if (!method2()) { return; }
+    
+    if (!method3()) { return; }
+    
+    if (!method4()) { return; }
+    
+    //提交操作
+    printf("提交数据");
+}
+
+bool method1() {
+    return NO;
+}
+
+bool method2() {
+    return YES;
+}
+
+bool method3() {
+    return NO;
+}
+
+bool method4() {
+    return YES;
+}
+
+
 @end
